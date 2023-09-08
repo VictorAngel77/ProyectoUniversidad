ea4995cd977672688db12c01d06bfce5171f57e4
VictorAngel77 <lx12858@innovaccion.mx>
2023-09-08 17:02:40 -0300

    Creacion de la clase Materia
    Nueva clase Materia, con constructor,get,set y toString

---
+++ Transversal/src/main/java/com/mycompany/transversal/Entidades/Materia.java
@@ -0,0 +1,63 @@
+
+package com.mycompany.transversal.Entidades;
+
+/**
+ *
+ * @author Victor Angel
+ */
+public class Materia {
+    private int idMateria=-1;
+    private String nombre;
+    private int año;
+    private boolean estado;
+
+    public Materia(String nombre, int año, boolean estado) {
+        this.nombre = nombre;
+        this.año = año;
+        this.estado = estado;
+    }
+
+    public Materia() {
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", a\u00f1o=" + año + ", estado=" + estado + '}';
+    }
+    
+    
+    
+    
+}

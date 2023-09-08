fd73868514b62e17fbea21f24863652064fe382e
Jose David Bardecio <120067068+Lilded23@users.noreply.github.com>
2023-09-08 16:50:33 -0300

    Update Alumno.java

--- Transversal/src/main/java/com/mycompany/transversal/Entidades/Alumno.java
+++ Transversal/src/main/java/com/mycompany/transversal/Entidades/Alumno.java
@@ -4,10 +4,71 @@
  */
 package com.mycompany.transversal.Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author jdbar
  */
 public class Alumno {
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNAc;
+    private boolean activo;
+
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNAc, boolean activo) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNAc = fechaNAc;
+        this.activo = activo;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNAc() {
+        return fechaNAc;
+    }
+
+    public void setFechaNAc(LocalDate fechaNAc) {
+        this.fechaNAc = fechaNAc;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNAc=" + fechaNAc + ", activo=" + activo + '}';
+    }
+    
     
 }

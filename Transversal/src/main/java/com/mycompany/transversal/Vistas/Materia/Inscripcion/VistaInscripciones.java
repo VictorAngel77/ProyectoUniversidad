4f5d3dccf42d901f8b2092c128d565c799a5ed90
cisco <cisco@e5-575g>
2023-09-27 13:36:57 -0300

    mala mia

--- Transversal/src/main/java/com/mycompany/transversal/Vistas/Materia/Inscripcion/VistaInscripciones.java
+++ Transversal/src/main/java/com/mycompany/transversal/Vistas/Materia/Inscripcion/VistaInscripciones.java
@@ -80,7 +80,7 @@ public class VistaInscripciones extends javax.swing.JPanel {
                         .fetchInscripcionesByIdAlumno(idAlumno)
                         .forEach(ins -> {
 
-                            if (!ins.toString().equals("null")) {
+                            if (!ins.toString().contains("null")) {
                                 modeloTabla.addRow(
                                         ins.toString().split(", ")
                                 );
